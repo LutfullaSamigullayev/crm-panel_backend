519e81718082da60c05cb4f1f30b3f8832efb23e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-13 12:25:56 +0500

    added student controller

---
+++ controller/student.controller.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express";
+
+export const getAllStudents = async (req:Request, res:Response, next: NextFunction): Promise<Response | void> => {
+    try {
+
+    } catch (error: any) {
+        next(error)
+    }
+}
+
+export const addStudent = async (req:Request, res:Response, next: NextFunction): Promise<Response | void> => {
+    try {
+
+    } catch (error: any) {
+        next(error)
+    }
+}
+export const updateStudent = async (req:Request, res:Response, next: NextFunction): Promise<Response | void> => {
+    try {
+
+    } catch (error: any) {
+        next(error)
+    }
+}
+export const deleteStudent = async (req:Request, res:Response, next: NextFunction): Promise<Response | void> => {
+    try {
+
+    } catch (error: any) {
+        next(error)
+    }
+}
