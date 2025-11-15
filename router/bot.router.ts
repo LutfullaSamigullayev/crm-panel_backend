f5dfbf855c90917f38a4e3db610365c97801121d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-15 22:05:13 +0500

    update bot router

--- router/bot.router.ts
+++ router/bot.router.ts
@@ -1,9 +1,11 @@
 import { Router, type RequestHandler } from "express";
-import { getAllMessages } from "../controller/bot.controller.js";
+import { getMessagesFromLastTenDays, getMessagesFromToday } from "../controller/bot.controller.js";
 
 const botRouter = Router()
 
-botRouter.get("/get_all_students", getAllMessages as RequestHandler)
+botRouter.get("/get_messages_from_today", getMessagesFromToday as RequestHandler)
+botRouter.get("/get_messages_from_last_ten_days", getMessagesFromLastTenDays as RequestHandler)
+
 
 export default botRouter
 
