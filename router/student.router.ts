9b097d9c07e21fd9caf481c5da1951f8b7388bbb
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-15 19:11:09 +0500

    fixed student router

--- router/student.router.ts
+++ router/student.router.ts
@@ -5,8 +5,8 @@ const studentRouter = Router()
 
 studentRouter.get("/get_all_students", getAllStudents as RequestHandler)
 studentRouter.post("/add_student", addStudent as RequestHandler)
-studentRouter.put("/update_student", updateStudent as RequestHandler)
-studentRouter.delete("/delete_student", deleteStudent as RequestHandler)
+studentRouter.put("/update_student/:id", updateStudent as RequestHandler)
+studentRouter.delete("/delete_student/:id", deleteStudent as RequestHandler)
 
 export default studentRouter
 
