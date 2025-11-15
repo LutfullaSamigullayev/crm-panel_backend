614aa981de993aaabfa1154b13b1328fc8ca1923
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-15 19:08:39 +0500

    fixed db added username

--- config/db.ts
+++ config/db.ts
@@ -5,10 +5,11 @@ dotenv.config()
 const sequelize = new Sequelize({
     dialect: "postgres",
     host: "localhost",
+    username: "postgres",
     port: 5432,
     password: String(process.env.DB_PASSWORD as string),
     database: String(process.env.DB_DATABASE as string),
-    logging: false
+    logging: false,
 })
 
 sequelize.authenticate().then(()=> console.log("Connected to DB")).catch((error) => console.log(error.message))
