e293848139d25d0b4ade8924f2c2a956de84598e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-15 20:09:25 +0500

    added bot router server.ts

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import studentRouter from "./router/student.router.js"
+import botRouter from "./router/bot.router.js"
 
 const app = express()
 dotenv.config()
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000
 
 // router
 app.use(studentRouter)
+app.use(botRouter)
 
 app.listen(PORT, () => {
     console.log("Server is running at: ", PORT);
